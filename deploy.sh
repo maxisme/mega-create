6e0784037648da48c181271b04d1aecfcc5c9482
Maximilian Mitchell <max@maxis.me>
2020-05-10 15:44:54 +0100

    fix mount

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export $(grep -v '^#' .env | xargs)
-rm -rf "mail/$DOMAIN/"
+mkdir mail
 echo '1@|{SHA512-CRYPT}$6$ie22FSkEZyEYY8vz$CD8OY2SWbJfgzYJ5BIiPk/YXS4cc4cyhEFrYRTNCA2tmUlQ.Zw1iywX5GBpG6Y93rIyL.Wf1ZOi1BOY1cw.UR.' >config/postfix-accounts.cf
 docker-compose up -d
 docker stack deploy -c stack.yml mega
