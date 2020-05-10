8e14cb951f6d7a29c3047a4f5b808934c4ed9a38
Maximilian Mitchell <max@maxis.me>
2020-05-10 14:21:33 +0100

    change bash script to check all users for account

--- upload/scripts/mega-create.sh
+++ upload/scripts/mega-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOMAIN="$DOMAINNAME"
-RETRIES=10
+RETRIES=20
 
 ##########
 # SCRIPT #
@@ -22,60 +22,65 @@ email="$username@$DOMAIN"
 # remove all inbox
 rm -rf "/var/mail/$DOMAIN/$username/*"
 
-sleep 5
+sleep 3
 
 # account creation with same password generated for email
 reg=$(megareg --register --email "$email" --name "John Doe" --password "$password")
 
 # get verify code part 1
 part1=$(echo "${reg}" | sed -n 3p)
+echo "$part1" > "/tmp/${username}.txt"
 
-echo "$part1" > "/var/mail/$DOMAIN/$username/part1.txt"
+sleep 3
 
 c=1
-while [[ $c -le $RETRIES ]]
-do
+while [[ $c -le $RETRIES ]]; do
   # look for verify email in inbox with verify code part2
-  for user in "/var/mail/$DOMAIN/*/"; do
-    if [ -d "/var/mail/$DOMAIN/$user/new/" ]; then
-      for i in "/var/mail/$DOMAIN/$user/new/"*; do
-        part1=$(cat /var/mail/$DOMAIN/$user/part1.txt)
+  for dir in "/var/mail/$DOMAIN/"*"/"; do
+    if [ -d "${dir}new/" ]; then
+      for i in "${dir}new/"*; do
+        user=${dir/\/var\/mail\/$DOMAIN\//}
+        user=${user/\//}
+        part1_file="/tmp/${user}.txt"
+        if [ -f "$part1_file" ]; then
+          part1=$(cat "$part1_file")
 
-        # get line number of https://mega.nz/#confirm
-        lineN=$(awk '/https:\/\/mega\.nz\/\#confirm/{ print NR; exit }' "$i")
-        # extract part2 of verification code
-        part2=$(sh -c "sed '$lineN!d' $i")
+          # get line number of https://mega.nz/#confirm
+          lineN=$(awk '/https:\/\/mega\.nz\/\#confirm/{ print NR; exit }' "$i")
+          # extract part2 of verification code
+          part2=$(sh -c "sed '$lineN!d' $i")
 
-        # use new domain
-        part2=${part2/mega.nz/mega.co.nz}
-        if [[ $part2 != *"mega"* ]]; then
-          exit 1
+          # use new domain
+          part2=${part2/mega.nz/mega.co.nz}
+          if [[ $part2 != *"mega"* ]]; then
+            exit 1
+          fi
+
+          email="$user@$DOMAIN"
+
+          # clean up
+          rm -rf "/var/mail/$DOMAIN/$user/" "$part1_file"
+
+          # run verifying code
+          verifyCODE=$(eval "${part1/@LINK@/$part2}")
+
+          if [[ $verifyCODE == *"Account registered successfully!"* ]]; then
+            echo "{\"email\": \"$email\", \"password\": \"$password\"}"
+            exit
+          else
+            echo "failed registration: $verifyCODE"
+            exit 1
+          fi
         fi
-
-        email="$user@$DOMAIN"
-
-        # clean up
-        rm -rf "/var/mail/$DOMAIN/$user/"
-        break
       done
     fi
   done
-  sleep 1
+  sleep 0.5
   let c=c+1
 done
 
-if [ -z "$part2" ]
-then
-  echo "Did not receive any confirmation emails in time."
-  exit 1
-fi
 
-# run verifying code
-verifyCODE=$(eval "${part1/@LINK@/$part2}")
+echo "Did not receive any confirmation emails in time."
+exit 1
 
-if [[ $verifyCODE == *"Account registered successfully!"* ]]; then
-  echo "{\"email\": \"$email\", \"password\": \"$password\"}"
-else
-  echo "failed registration: $verifyCODE"
-  exit 1
-fi
+
