43031d2ab0b31a039dcd6b0ed85a7b99a12425dc
Maximilian Mitchell <max@maxis.me>
2020-05-10 15:24:12 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export $(grep -v '^#' .env | xargs)
 rm -rf "mail/$DOMAIN/"
-> config/postfix-accounts.cf
+>config/postfix-accounts.cf
+git pull
 docker-compose up -d
 docker stack deploy -c stack.yml mega
