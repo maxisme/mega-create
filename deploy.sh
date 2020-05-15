7786df7e155195e673b13ac7b5da7ccdc3913413
Maximilian Mitchell <max@maxis.me>
2020-05-15 21:53:56 +0100

    remove unused ports

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 export $(grep -v '^#' .env | xargs)
 mkdir mail
 echo '1@|{SHA512-CRYPT}$6$ie22FSkEZyEYY8vz$CD8OY2SWbJfgzYJ5BIiPk/YXS4cc4cyhEFrYRTNCA2tmUlQ.Zw1iywX5GBpG6Y93rIyL.Wf1ZOi1BOY1cw.UR.' >config/postfix-accounts.cf
-docker-compose up -d
+docker-compose up -d --build
 docker stack deploy -c stack.yml mega
